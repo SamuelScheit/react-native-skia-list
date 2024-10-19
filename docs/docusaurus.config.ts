ad966104721fdd58d574d995e463ac48f4f68b93
samuelscheit <github@samuelscheit.com>
2024-10-19 15:00:33 +0200

    feat: ci/cd docs deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
 	url: "https://samuelscheit.github.com",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: process.env.NODE_ENV === "production" ? "/react-native-skia-list/" : "/",
+	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
